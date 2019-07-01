994b297c338f3ecd13a9c99a2fe0435d068b9513
sikaili99 <mathewsmusukuma@outlook.com>
2019-07-01 21:46:24 +0200

    Changed a route

--- cats/urls.py
+++ cats/urls.py
@@ -4,6 +4,6 @@ from . import views  # Import view from the current folder
 # Here I have created routes of the app
 urlpatterns = [
     path('', views.index),
-    path('cat/cute/', views.search,  name='search'),
+    path('cat/says/', views.search,  name='search'),
 ]
 
