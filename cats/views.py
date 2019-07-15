19865f99f293d04ef9683ddba35df2698c971f74
sikaili99 <mathewsmusukuma@outlook.com>
2019-07-15 22:37:33 +0200

    Able to fetch cats by tag  from the cataas API

--- cats/views.py
+++ cats/views.py
@@ -8,15 +8,19 @@ from .models import Cats
 
 def index(resquest):
 
-    cats = Cats.objects.all()
-    return render(resquest, 'index.html', {'cats': cats})
+    url = requests.get('https://cataas.com/api/cats?limit=10')
+    response = url.json()
+    return render(resquest, 'index.html', {'cats': response})
 
 
 def search(request):
     search_tag = request.GET.get('q')
     if request.method == 'GET' and search_tag != '':
 
-        return render(request, 'search.html', {'tag': search_tag})
+        url = requests.get(f'https://cataas.com/api/cats?tags={search_tag}')
+        search_res = url.json()
+        print(search_res)
+        return render(request, 'search.html', {'results_tag': search_res})
 
     else:
         return render(request, 'search.html', {'message': 'Enter a tag to search for a cat'})
