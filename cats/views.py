497cd92f4962dead9733fa83de69a4ec7927ce30
Mathewsmusukuma <sikaili99@gmail.com>
2020-02-10 14:48:57 +0200

    Updates

--- cats/views.py
+++ cats/views.py
@@ -16,11 +16,15 @@ def search(request):
     if request.method == 'GET' and search_tag != '':
 
         url = requests.get(f'{my_url}api/cats?tags={search_tag}')
-        search_res = url.json()
-        return render(request, 'search.html', {'results_tag': search_res})
+        search_result = url.json()
+        not_found = f"No cat found with your tag key word '{search_tag}'"
+        if search_result:
+            return render(request, 'search.html', {'results_tag': search_result})
+        
+        return render(request, 'search.html', {'message': not_found})
 
     else:
-        return render(request, 'search.html', {'message': 'Enter a tag to search for a cat'})
+        return render(request, 'search.html', {'message': 'Enter a tag to search for a cat, e.g Jump'})
 
 
 def view_cat(request, cat_id):
